79bba6f71f5957d867f0b90b35fe53d1370852c1
HectorHW <vsredkin01@gmail.com>
2021-10-21 00:26:14 +0300

    pretty printing chunk with arrows for jumps

--- src/execution/chunk.rs
+++ src/execution/chunk.rs
@@ -82,9 +82,31 @@ impl AddAssign<Opcode> for Chunk {
 
 impl Display for Chunk {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for (i, opcode) in self.code.iter().enumerate() {
-            writeln!(
-                f,
+        let strings = chunk_pretty_printer::draw_chunk(self);
+        for s in &strings {
+            writeln!(f, "{}", s)?
+        }
+        Ok(())
+    }
+}
+
+mod chunk_pretty_printer {
+    use crate::execution::chunk::{Chunk, Opcode};
+
+    pub fn draw_chunk(chunk: &Chunk) -> Vec<String> {
+        let mut strings = draw_instructions(chunk);
+        extend_canvas(&mut strings);
+        draw_arrows(chunk, &mut strings);
+        strings
+    }
+
+    fn draw_instructions(chunk: &Chunk) -> Vec<String> {
+        use std::fmt::Write;
+        let mut res = vec![];
+        for (i, opcode) in chunk.code.iter().enumerate() {
+            let mut s = String::new();
+            write!(
+                s,
                 "{:<5} {}",
                 i,
                 match opcode {
@@ -92,7 +114,7 @@ impl Display for Chunk {
                         format!(
                             "{:<21} (value {})",
                             format!("{}", Opcode::LoadConst(*idx)),
-                            (self.constants[(*idx) as usize])
+                            (chunk.constants[(*idx) as usize])
                         )
                     }
 
@@ -119,8 +141,68 @@ impl Display for Chunk {
                         format!("{}", any_other)
                     }
                 }
-            )?
+            );
+            res.push(s);
         }
-        Ok(())
+        return res;
+    }
+
+    fn extend_canvas(lines: &mut Vec<String>) {
+        for item in lines {
+            let mut new_str = String::new();
+            new_str.push_str("  ");
+            new_str.push_str(item);
+            *item = new_str;
+        }
+    }
+
+    fn draw_arrows(chunk: &Chunk, lines: &mut Vec<String>) {
+        let arrows = chunk
+            .code
+            .iter()
+            .enumerate()
+            .filter_map(|(pos, opcode)| match opcode {
+                Opcode::JumpIfFalse(delta) | Opcode::Jump(delta) => {
+                    let mut start = pos;
+                    let mut end = start + *delta as usize;
+                    if start > end {
+                        std::mem::swap(&mut start, &mut end);
+                    }
+                    Some((start, end))
+                }
+                _ => None,
+            })
+            .collect::<Vec<_>>();
+
+        for (arrow_start, arrow_end) in arrows {
+            if !check_canvas_range(lines, (arrow_start, arrow_end)) {
+                extend_canvas(lines);
+            }
+
+            //draw exit
+
+            draw_at_char(&mut lines[arrow_start], 0, b'*');
+            for i in arrow_start + 1..arrow_end {
+                draw_at_char(&mut lines[i], 0, b'|');
+            }
+            draw_at_char(&mut lines[arrow_end], 0, b'>');
+        }
+    }
+
+    fn check_canvas_range(lines: &Vec<String>, range: (usize, usize)) -> bool {
+        for i in range.0..=range.1 {
+            if !lines[i].starts_with(' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn draw_at_char(s: &mut String, idx: usize, c: u8) {
+        if !c.is_ascii() {
+            panic!()
+        }
+        let raw_str_ref = unsafe { s.as_bytes_mut() };
+        raw_str_ref[idx] = c;
     }
 }
