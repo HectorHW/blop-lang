7410fc798a1ad7114765474f2d7be232b419a5bf
HectorHW <github@hectorhw.net>
2021-10-20 15:00:32 +0300

    accept file to run as argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::compile::compiler::Compiler;
 use crate::execution::vm::VM;
+use std::env;
 
 mod compile;
 mod data;
@@ -7,7 +8,13 @@ mod execution;
 mod parsing;
 
 fn main() {
-    let file_content = std::fs::read_to_string("input.txt").expect("failed to read file");
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        println!("please provide file to run as argument.");
+        return;
+    }
+    let filename = args.get(1).unwrap();
+    let file_content = std::fs::read_to_string(filename).expect("failed to read file");
 
     //normalize string - make all lines joined with \n (incase running on windows)
     let file_content = file_content.lines().collect::<Vec<_>>().join("\n");
